7076b8db8f731ef9cb2ce16f0d183279b0fe75a1
Joknaa <54961980+Joknaa@users.noreply.github.com>
2022-02-05 17:57:30 +0100

    Added the Hindi version of the dialoge
    Added the Hindi version of the dialogue, and a menu that shows at the end, with 2 options; Restart the game, and Quite the game

--- Assets/Taziri/Scripts/MenuManager.cs
+++ Assets/Taziri/Scripts/MenuManager.cs
@@ -1,22 +1,49 @@
 ﻿using Fungus;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Taziri.Scripts {
     public class MenuManager : MonoBehaviour {
         [SerializeField] private Flowchart flowchart;
-        [SerializeField] private GameObject MainMenu;
+        [SerializeField] private Character you;
+        [SerializeField] private Character mery;
+        [SerializeField] private Character narrator;
+        [SerializeField] private GameObject mainMenu;
+        [SerializeField] private GameObject finishMenu_English;
+        [SerializeField] private GameObject finishMenu_Hindi;
 
-    
 
         public void SelectEnglish() {
             flowchart.SetStringVariable("Language", "English");
+            you.name = "You";
+            mery.name = "Mery";
+            narrator.name = "Narrator";
         }
+
         public void SelectHindi() {
             flowchart.SetStringVariable("Language", "Hindi");
+            you.name = "आप";
+            mery.name = "मेरी";
+            narrator.name = "कथावाचक";
         }
 
         public void ConfirmSelection() {
-            MainMenu.SetActive(false);
+            mainMenu.SetActive(false);
+        }
+
+        public void DisplayFinishMenu_English() {
+            finishMenu_English.SetActive(true);
+        }
+        public void DisplayFinishMenu_Hindi() {
+            finishMenu_Hindi.SetActive(true);
+        }
+
+        public void Restart() {
+            SceneManager.LoadScene(0);
+        }
+
+        public void Quite() {
+            Application.Quit();
         }
     }
 }
